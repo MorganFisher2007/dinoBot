973bc4ccb129d8a768b512d25e8a4ab3f6f3da91
MorganFisher2007 <morgantf2000@gmail.com>
2022-04-20 04:56:14 -0700

    working, further work would need to be done to decrease training time

--- bot.py
+++ bot.py
@@ -5,16 +5,25 @@ from torch import float64
 import torchvision
 import numpy as np
 from PIL import Image
+import stable_baselines3
 from selenium import webdriver
+from concurrent.futures import ThreadPoolExecutor
 import matplotlib.pyplot as plt
-from gym.spaces import Discrete, Box
+from stable_baselines3 import DQN
 from torchvision import transforms
+from gym.envs.registration import register 
+from gym.spaces import Discrete, Box, Tuple
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import JavascriptException
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.evaluation import evaluate_policy
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
@@ -24,9 +33,16 @@ class Env(gym.Env):
     
     def __init__(self):
         self.driver = self.init_driver()
+        self.done = True
+        self.reset()
+        obs = self.take_screenshot()
         self.action_space = Discrete(4)
-        self.observation_space = Box(0.0, 1.0, shape=(225, 360), dtype=np.float64)
-    
+        self.observation_space = Box(low=0.0, high=1.0, shape=obs.shape, dtype=obs.dtype)
+        try:
+            self.driver.get('chrome://dino/')
+        except WebDriverException:
+            pass
+
     def take_screenshot(self):
         path = os.path.abspath(os.getcwd()) + '/screenshot.png'
         self.driver.get_screenshot_as_file(path)
@@ -35,25 +51,21 @@ class Env(gym.Env):
             transforms.Grayscale(), 
             transforms.ToTensor()])
         os.remove(path)
-        return np.squeeze(transform(img).numpy())
+        return np.ndarray.flatten(transform(img).numpy())
     
     def render(self):
-        VISU = True
+        pass
     
     def reset(self):
+        if not self.done:
+            return
         if self.driver.current_url == 'chrome://dino/':
+            print('refreshed')
             self.driver.refresh()
-        else:
-            try:
-                self.driver.get('chrome://dino/')
-            except WebDriverException:
-                pass
         self.driver.set_window_size(360, 225)
         self.driver.execute_script("document.visibilityState = 'visible';")
         ActionChains(self.driver).key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-        self.done = False
         self.driver.execute_script('console.clear()')
-        start = time.time()
         self.observation = self.take_screenshot()
         return self.observation
 
@@ -63,32 +75,24 @@ class Env(gym.Env):
     
     def init_driver(self):
         print('Starting driver...', end='', flush=True)
-        
         options = webdriver.ChromeOptions()
-        if Env.VISU:
-            options.add_argument('--kiosk')
-            options.add_argument("--incognito")
-            options.add_argument('disable-infobars')
-            options.add_argument('disable-extensions')
-            options.add_argument('disable-notifications')
-            options.add_experimental_option("useAutomationExtension", False)
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        else: options.add_argument('headless')
+        if not Env.VISU:
+            options.add_argument('headless')
+        #options.add_argument('--kiosk')
+        #options.add_argument("--incognito")
+        #options.add_argument('disable-infobars')
+        #options.add_argument('disable-extensions')
+        #options.add_argument('disable-notifications')
+        #options.add_experimental_option("useAutomationExtension", False)
+        #options.add_experimental_option("excludeSwitches", ["enable-automation"])
         caps = DesiredCapabilities.CHROME
         caps['goog:loggingPrefs'] = { 'browser':'ALL' }
         driver = webdriver.Chrome(options = options, desired_capabilities = caps)
 
         print('OK')
         return driver
-       
-    def update_done(self):
-        self.driver.execute_script('console.log(Runner.instance_.playing);')
-        if 'true' in dict(self.driver.get_log('browser')[-1])['message']:
-            self.done = False
-        else: self.done = True
-        self.driver.execute_script('console.clear()')
     
-    def step(self, action: int): # add default value of the action (no keyboard clicks)
+    def step(self, action: int):
         if action == 0:
             pass
         elif action == 1:
@@ -97,25 +101,33 @@ class Env(gym.Env):
             ActionChains(self.driver).key_down(Keys.ARROW_DOWN).perform()
         elif action == 3: 
             ActionChains(self.driver).key_up(Keys.ARROW_DOWN).perform()
-        self.update_done()
+        try: self.driver.execute_script('console.log(Runner.instance_.playing);')
+        except JavascriptException:
+            pass
+        if 'true' in dict(self.driver.get_log('browser')[-1])['message']:
+            self.done = False
+        else: self.done = True
+        self.driver.execute_script('console.clear()')
         self.observation = self.take_screenshot()
-        self.reward = self.compute_reward(self.observation)
-        self.info = None
+        self.driver.execute_script('console.log(Runner.instance_.currentSpeed);')
+        speed = self.driver.get_log('browser')[-1]['message']
+        self.reward = float(speed[17:]) - 10.0
+        self.info = {}
         return self.observation, self.reward, self.done, self.info
-    
-    def compute_reward(self, observation):
-        pass
+
 
 if __name__ == '__main__':
-    env = Env()
-    for i_episode in range(20):
-        observation = env.reset()
-        for t in range(100):
-            #env.render()
-            #print(observation)
-            action = env.action_space.sample()
-            observation, reward, done, info = env.step(action)
-            if done:
-                #print("Episode finished after {} timesteps".format(t+1))
-                break
-env.close()
+    with ThreadPoolExecutor(max_workers=8):
+        env = make_vec_env(lambda: Env(), n_envs=1, vec_env_cls=DummyVecEnv)
+    model = DQN('MlpPolicy', env, verbose=10)
+    model.learn(total_timesteps=int(2e5))
+    model.save("dinoTest1")
+    del model
+    model = DQN.load("dinoTest1", env=env)
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+    obs = env.reset()
+    for i in range(1000):
+        action, _states = model.predict(obs, deterministic=False)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
